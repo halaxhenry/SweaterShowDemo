28f88825cbe3c876de6e7d0abb4b9b9aec52c2b4
halaxhenry <halaxhenry@gmail.com>
2022-03-08 17:33:53 +0900

    Initial Commit

---
+++ SweaterShowDemo/SweaterShowDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShowDemoApp.swift
+//  SweaterShowDemo
+//
+//  Created by Seungchul Ha on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShowDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
